b0524a875f0a0e03483a9a8eb21037076092db33
KietLe <letuankiet26102003kl@gmail.com>
2024-08-08 22:03:21 +0700

    Edit Assgiment 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { useState } from "react";
 import "./App.css";
 import NavBar from "./layout/NavBar";
 import HomePage from "./pages/homePage";
 import ListCatLeg from "./component/ListCatLeg";
 
 function App() {
-  const [active, setIsActive] = useState<boolean>(false);
+
   return (
     <div className="">
         <NavBar/>
